88b8edfd3aedf638a24969da10f3a05f79fe2c44
ivan.cardoso <ivan.cardoso@itbam.org.br>
2020-02-15 11:33:26 -0400

    remote api

--- src/enviroment.js
+++ src/enviroment.js
@@ -1,2 +1,2 @@
-export const API_PATH = 'http://localhost:8080';
-// export const API_PATH = 'https://proex-api.herokuapp.com';
+// export const API_PATH = 'http://localhost:8080';
+export const API_PATH = 'https://proex-api.herokuapp.com';
